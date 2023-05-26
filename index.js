b70e08d3d60772654c78a38fa8ea6ab0fa5d557a
ProfSDas <132560711+ProfSDas@users.noreply.github.com>
2023-05-26 19:20:47 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+function redirectToFillPage() {
+
+  window.location.href="fill.html";
+
+}
+
+function redirectToResponseSignupPage() {
+
+  window.location.href="responsesignup.html";
+
+}
+
+function redirectToLoginPage() {
+
+  window.location.href="login.html";
+
+}
+
+function redirectToSignupPage() {
+
+  window.location.href="signup.html";
+
+}
+
+function redirectToContactPage() {
+
+  window.location.href="contact.html";
+
+}
