8b80ee67761d808202d15d3ca0a9350bd09c52fc
ProfSDas <132560711+ProfSDas@users.noreply.github.com>
2023-05-26 17:30:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-// Get the elements we need
-const navbar = document.querySelector('.navbar');
-const menu = document.querySelector('.menu');
-const burger = document.querySelector('.burger');
-
-// Add a click event listener to the burger icon
-burger.addEventListener('click', () => {
-  // Toggle the "active" class on the burger icon
-  burger.classList.toggle('burger');
-  // Toggle the "active" class on the menu
-  menu.classList.toggle('menu');
-});
-
-// Hide the menu when a link is clicked
-menu.addEventListener('click', () => {
-  menu.classList.remove('menu');
-  burger.classList.remove('burger');
-});
-
 function redirectToFillPage() {
   window.location.href="/fill/index.html";
 }
