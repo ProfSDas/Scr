cbd74749ed2784763c0ab99742b4b37b6a7a763c
ProfSDas <132560711+ProfSDas@users.noreply.github.com>
2023-05-26 19:20:02 +0530

    Create index2.js

---
+++ index2.js
@@ -0,0 +1,16 @@
+
+function redirectToFillPage() {
+  window.location.href="fill.html";
+}
+function redirectToResponseSignupPage() {
+  window.location.href="responsesignup.html";
+}
+function redirectToLoginPage() {
+  window.location.href="login.html";
+}
+function redirectToSignupPage() {
+  window.location.href="signup.html";
+}
+function redirectToContactPage() {
+  window.location.href="contact.html";
+}
